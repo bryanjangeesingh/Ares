3e35be8072c9dc4b16b471c344c53d2b77290234
Jonas Rajagopal <jrajagop@mit.edu>
2024-11-24 19:42:22 -0500

    Reviewed averaging

--- hdl/average.sv
+++ hdl/average.sv
@@ -1,8 +1,8 @@
 module average #(
         parameter integer C_S00_AXIS_TDATA_WIDTH  = 32,
         parameter integer C_M00_AXIS_TDATA_WIDTH  = 32,
-        parameter integer SAMPLES_PER_TRIGGER = 1000,
-        parameter integer AVERAGES = 100
+        parameter integer SAMPLES_PER_TRIGGER = 1024,
+        parameter integer AVERAGES = 128
     )
     (
     input wire  s00_axis_aclk, s00_axis_aresetn, // ADC axis
@@ -18,7 +18,7 @@ module average #(
     output logic [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata,
     output logic [(C_M00_AXIS_TDATA_WIDTH/8)-1: 0] m00_axis_tstrb);
 
-    typedef enum {IDLE=0, COLLECTING=1, AVERAGING=2, OUTPUTTING=3} state_t;
+    typedef enum {IDLE=0, COLLECTING=1, AVERAGING=2} state_t;
 
     state_t state;
 
@@ -28,41 +28,27 @@ module average #(
     logic prev_valid_in;         
     
     // Memory for accumulation
-    logic [47:0] accumulator [SAMPLES_PER_TRIGGER-1:0];  // each individual sample is 48 bits
-    logic [31:0] averaged_data [SAMPLES_PER_TRIGGER-1:0]; 
+    logic [31:0] accumulator [SAMPLES_PER_TRIGGER-1:0];  // each individual sample is 48 bits
     
-    logic [9:0] write_addr, read_addr;
-    logic write_enable;
-    logic [31:0] holding_buffer;
-    
-    assign s00_axis_tready = (state == COLLECTING);
+    assign s00_axis_tready = 1;
     assign m00_axis_tstrb = 4'b1111;
 
     always_ff @(posedge s00_axis_aclk) begin
         if (~s00_axis_aresetn) begin
             state <= IDLE;
-            sample_counter <= 0;
-            trigger_counter <= 0;
-            output_counter <= 0;
-            write_enable <= 0;
-            m00_axis_tvalid <= 0;
-            m00_axis_tlast <= 0;
             prev_valid_in <= 0;
-            
-            // Reset the accumulator and averaged data
-            for (int i = 0; i < SAMPLES_PER_TRIGGER; i++) begin
-                accumulator[i] <= 0;
-                averaged_data[i] <= 0;
-            end
+            trigger_counter <= 0;
+
         end else begin
             prev_valid_in <= s00_axis_tvalid;
-            
             case (state)
                 IDLE: begin
                     if (s00_axis_tvalid && ~prev_valid_in) begin
                         state <= COLLECTING;
                         sample_counter <= 0;
-                        write_enable <= 1; // start writing to memory
+                    end else begin
+                        m00_axis_tvalid <= 0;
+                        m00_axis_tlast <= 0;
                     end
                 end
                 
@@ -78,7 +64,7 @@ module average #(
                             
                             if (trigger_counter == AVERAGES-1) begin
                                 state <= AVERAGING;
-                                write_enable <= 0;
+                                output_counter <= 0;
                             end else begin
                                 state <= IDLE;
                             end
@@ -87,56 +73,29 @@ module average #(
                 end
                 
                 AVERAGING: begin
-                    // Perform averaging for all samples
-                    for (int i = 0; i < SAMPLES_PER_TRIGGER; i++) begin
-                        averaged_data[i] <= accumulator[i] / AVERAGES;
-                        accumulator[i] <= 0; // Reset accumulator at that index 
-                    end
-                    output_counter <= 0;
-                    trigger_counter <= 0;
-                    state <= OUTPUTTING;
-                    m00_axis_tvalid <= 1;
-                end
-                
-                OUTPUTTING: begin
+                    // Perform averaging and output for all samples
                     if (m00_axis_tready) begin
-                        m00_axis_tdata <= averaged_data[output_counter];
+                        m00_axis_tvalid <= 1;
+                        m00_axis_tdata <= accumulator[output_counter] >> 7;
                         output_counter <= output_counter + 1;
-                        
-                        if (output_counter == SAMPLES_PER_TRIGGER-1) begin
+                        if (output_counter == SAMPLES_PER_TRIGGER-2) begin
                             m00_axis_tlast <= 1;
-                            state <= IDLE;
-                            m00_axis_tvalid <= 0;
-                        end else begin
-                            m00_axis_tlast <= 0;
                         end
+                        if (output_counter == SAMPLES_PER_TRIGGER-1) begin
+                            state <= IDLE;
+                            m00_axis_tlast <= 0;
+                            trigger_counter <= 0;
+                            m00_axis_tvalid <= 0;
+                        end
+                    end else begin
+                        m00_axis_tvalid <= 0;
                     end
+                end
+
+                default: begin
+                    state <= IDLE;
                 end
             endcase
         end
     end
-
-    xilinx_true_dual_port_read_first_2_clock_ram #(
-        .RAM_WIDTH(32),
-        .RAM_DEPTH(1024)
-    ) fifo_buffer (
-        .addra(write_addr),
-        .clka(s00_axis_aclk),
-        .wea(write_enable),
-        .dina(s00_axis_tdata),
-        .ena(1'b1),
-        .regcea(1'b1),
-        .rsta(s00_axis_aresetn),
-        .douta(),
-
-        .addrb(read_addr),
-        .dinb(),
-        .clkb(m00_axis_aclk),
-        .web(1'b0),
-        .enb(1'b1),
-        .regceb(1'b1),
-        .rstb(m00_axis_aresetn),
-        .doutb(holding_buffer)
-    );
-
 endmodule
